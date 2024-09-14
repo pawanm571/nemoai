cb1ba465e5ed080e25f4442c92b58cecfdcf74b3
pawanm571 <43287809+pawanm571@users.noreply.github.com>
2024-09-14 17:05:31 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -102,7 +102,7 @@ def load_modelvision() -> genai.GenerativeModel:
     The function `load_modelvision` loads a generative model for vision tasks using the `gemini-pro-vision` model.
     :return: an instance of the `genai.GenerativeModel` class.
     """
-    model = genai.GenerativeModel('gemini-pro-vision')
+    model = genai.GenerativeModel('gemini-1.5-flash')
     return model
 
 
